b88a84addea29568bd131566d7c3f9a6c46e4204
bennet <bennet.outland@protonmail.com>
2023-04-01 09:09:22 -0600

    data collection

---
+++ src/csv_injest.jl
@@ -0,0 +1,12 @@
+using CSV
+using DataFrames
+
+# Load CSV Files for Gasses
+
+# R-134a (NIST)
+R134a = CSV.File("GasData/R134a.csv")
+
+# CO2 (NIST)
+CO2 = CSV.File("GasData/CO2.csv")
+
+print(typeof(CO2))

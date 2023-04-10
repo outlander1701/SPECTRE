ba9adc325808deed3b9bc3d6576b412625dea194
Joshua <jhoff2021@gmail.com>
2023-04-10 12:24:14 -0600

    new file:   GasData/R134a_Mix_Sat2.csv 	new file:   GasData/R134a_Mix_Sat2.txt 	new file:   InsaneResolutionPlot.svg 	modified:   src/main.jl 	modified:   src/plotting.jl

--- src/main.jl
+++ src/main.jl
@@ -22,7 +22,7 @@ T_evap = 248.0 # K Need to look at this value
 P_evap = 0.10655 # MPa this is just for R-134a
 h_evap = 383.4 # kJ/kg
 s_evap = 1.7461 # kJ/kgK
-P_mix = 0.05:0.001:0.106;
+P_mix = 0.05:0.0001:0.106;
 
 State_1 = State(T_evap, P_evap, h_evap, s_evap, 1.0)
 State_9 = State(T_cond, P_cond, h_cond, s_cond, 0.0)
@@ -40,5 +40,5 @@ println(CoP)
 println(" ")
 """
 
-work_out_net_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L)
+#work_out_net_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L)
 CoP_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L)

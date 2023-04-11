ab240995377ae17c38b873e07aaa251e17b81cc3
Joshua <jhoff2021@gmail.com>
2023-04-11 12:59:56 -0600

    new file:   GasData/CO2/CO2_P_max.csv 	new file:   GasData/CO2/CO2_P_max.txt 	modified:   src/csv_injest.jl 	modified:   src/cycles.jl 	modified:   src/main.jl

--- src/csv_injest.jl
+++ src/csv_injest.jl
@@ -14,15 +14,14 @@ R134a_Dif = CSV.File("GasData/R134a/R134a_P4_Data.csv")
 R134a = Dict("Comp" => R134a_Comp, "Cond" => R134a_Cond, "Throttle" => R134a_Throttle, "Evap" => R134a_Evap, "Mix" => R134a_Mix, "Dif" => R134a_Dif, "name" => "R-134a")
 
 # CO2 (NIST)
-"""
-CO2_Comp = CSV.File("GasData/CO2/CO2_Pmax_SH.csv")
-CO2_Cond = CSV.File("GasData/CO2/CO2_Pmax_Sat.csv")
+
+CO2_Comp = CSV.File("GasData/CO2/CO2_P_max.csv")
+CO2_Cond = CSV.File("GasData/CO2/CO2_P_max.csv")
 CO2_Throttle = CSV.File("GasData/CO2/CO2_Evap.csv")
 CO2_Evap = CSV.File("GasData/CO2/CO2_Evap.csv")
 CO2_Mix = CSV.File("GasData/CO2/CO2_Mix.csv")
 CO2_Dif = CSV.File("GasData/CO2/CO2_Sat_Data.csv")
 
 CO2 = Dict("Comp" => CO2_Comp, "Cond" => CO2_Cond, "Throttle" => CO2_Throttle, "Evap" => CO2_Evap, "Mix" => CO2_Mix, "Dif" => CO2_Dif, "name" => "CO_{2}")
-"""
 
 Gasses = [R134a]
--- src/main.jl
+++ src/main.jl
@@ -22,17 +22,31 @@ T_evap = 248.0 # K Need to look at this value
 P_evap = 0.10655 # MPa this is just for R-134a
 h_evap = 383.4 # kJ/kg
 s_evap = 1.7461 # kJ/kgK
+P_mix = 0.05:0.001:0.106;
+#P_mix = 0.08;
+
+"""
+P_cond = 7.3783; # MPa
+T_cond = 308.15; # K
+h_cond = 403.36; # kJ/kg
+s_cond = 1.6668; # kJ/kgK
+
+T_evap = 249.01 # K Need to look at this value
+P_evap = 1.7297 # MPa this is just for R-134a
+h_evap = 437.06 # kJ/kg
+s_evap = 1.9690 # kJ/kgK
 #P_mix = 0.05:0.001:0.106;
-P_mix = 0.08
+P_mix = 1.7;
+"""
 
 State_1 = State(T_evap, P_evap, h_evap, s_evap, 1.0)
 State_9 = State(T_cond, P_cond, h_cond, s_cond, 0.0)
 
-m_dot_1, m_dot_9, work, CoP, Ψ = SPECTRE(State_1, State_9, Gasses[1], P_mix, Q_L, T_L, T_H)
+#m_dot_1, m_dot_9, work, CoP, Ψ = SPECTRE(State_1, State_9, Gasses[1], P_mix, Q_L, T_L, T_H)
 #m_dot_1, work, CoP, Ψ = Simple_Throttle(State_1, State_9, Gasses[1], P_mix, Q_L, T_L, T_H)
 #m_dot_1, work, CoP, Ψ = Simple_Turbine(State_1, State_9, Gasses[1], P_mix, Q_L, T_L, T_H)
 
-
+"""
 println(" ")
 println(m_dot_1)
 println(m_dot_9)
@@ -40,7 +54,7 @@ println(work)
 println(CoP)
 println(Ψ)
 println(" ")
+"""
 
-
-#work_in_net_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L,T_L, T_H)
+work_in_net_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L,T_L, T_H)
 #CoP_vs_pmix(SPECTRE, State_1, State_9, Gasses, P_mix, Q_L)
